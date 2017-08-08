1c26c32592362572697f3f0bba2fd8d8469674c2
Tobias Braendli <tobiasbraendli@gmail.com>
2017-08-08 13:30:35 +0100

    done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,20 +10,38 @@ class TasksController < ApplicationController
   #SINGULAR!!
   end
 
-  def new
-    @task = Task.new
-  end
+    def new
+      @task = Task.new
+    end
 
-  def create
-  end
+    def edit
+      @task = Task.find(params[:id])
+    end
 
-  def edit
-  end
 
-  def update
-  end
+    def create
+      @task = Task.new(task_params)
+      @task.save
+      redirect_to task_path(@task)
+    end
+
+    def update
+      @task = Task.find(params[:id])
+      @task.update(task_params)
+      redirect_to task_path(@task)
+    end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :detail)
   end
 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-  validates :name, presence: true
-  validates :detail, presence: true
+  # validates :name, presence: true
+  # validates :detail, presence: true
 end
